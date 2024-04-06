0f2c8dcffaa7a3964e05da4704154c8a5412c8bd
thiago laredo <thiago@olatu.com.br>
2024-04-06 16:58:26 -0300

    submenu animado no mobile

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,9 +1,12 @@
 import outsideClick from "./outsideclick.js";
 
 export default class MenuMobile {
-  constructor(menuButton, menuList, events) {
+  constructor(logoMobile, menuButton, menuList, contatoMobile, linkedinMobile, events) {
+    this.logoMobile = document.querySelector(logoMobile);
     this.menuButton = document.querySelector(menuButton);
     this.menuList = document.querySelector(menuList);
+    this.contatoMobile = document.querySelector(contatoMobile);
+    this.linkedinMobile = document.querySelector(linkedinMobile);
     this.activeClass = "active";
 
     // define touchstart e click como argumento padrão
@@ -22,18 +25,38 @@ export default class MenuMobile {
 
   openMenu(event) {
     event.preventDefault();
+    this.logoMobile.classList.add(this.activeClass);
     this.menuList.classList.add(this.activeClass);
     this.menuButton.classList.add(this.activeClass);
+    this.contatoMobile.classList.add(this.activeClass);
+    this.linkedinMobile.classList.add(this.activeClass);
+
+    this.animateElementsSequentially();
+
+
     outsideClick(this.menuList, this.events, () => {
-      this.menuList.classList.remove(this.activeClass);
-      this.menuButton.classList.remove(this.activeClass);
+    this.closeMenu();
     });
   }
+
+  closeMenu(event) {
+    this.logoMobile.classList.remove(this.activeClass);
+    this.menuList.classList.remove(this.activeClass);
+    this.menuButton.classList.remove(this.activeClass);
+    this.contatoMobile.classList.remove(this.activeClass);
+    this.linkedinMobile.classList.remove(this.activeClass);
+
+  }
+
 
   addMenuMobileEvents() {
     this.events.forEach((evento) =>
       this.menuButton.addEventListener(evento, this.openMenu)
     );
+    this.menuList.addEventListener('click', (event) => {
+      console.log('Menu list item clicked');
+      this.closeMenu();
+    });
   }
 
   addLinkClickEvents() {
@@ -63,10 +86,21 @@ export default class MenuMobile {
   });
 }
 
-  
-  
+animateElementsSequentially() {
+  const elementsToAnimate = [
+    ...this.menuList.querySelectorAll('li'),
+    this.contatoMobile,
+    this.linkedinMobile
+  ];
+
+  elementsToAnimate.forEach((element, index) => {
+    // Defina a duração da animação e o delay baseado na posição do elemento na sequência
+    element.style.animation = `fadeIn 0.5s forwards ${index * 0.3}s`;
+  });
+}
+
   init() {
-    if (this.menuButton && this.menuList) {
+    if (this.logoMobile && this.menuButton && this.menuList  && this.contatoMobile && this.linkedinMobile) {
       this.addMenuMobileEvents();
       this.addLinkClickEvents(); 
     }
