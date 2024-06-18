5b52618d60f297efbcf48c52a91a5c483e0914dc
thiago laredo <thiago@olatu.com.br>
2024-06-18 11:45:06 -0300

    form funcionando

--- src/server.mjs
+++ src/server.mjs
@@ -1,23 +1,24 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import nodemailer from 'nodemailer';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 5000; // Mude a porta para 5000
+const port = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/send-email', (req, res) => {
-  console.log("Recebendo requisição:", req.body);
   const { nome, email, whatsapp, motivo, mensagem } = req.body;
 
   let transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'mail.myaswimwear.com.br',
+    port: 587,
+    secure: false,  // true for 465, false for other ports
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
@@ -25,8 +26,8 @@ app.post('/send-email', (req, res) => {
   });
 
   const mailOptions = {
-    from: email,
-    to: 'olatuthinking@gmail.com',
+    from: process.env.EMAIL_USER,
+    to: 'olatuthinking@gmail.com  ',
     subject: `Mensagem de ${nome}`,
     text: `Nome: ${nome}\nEmail: ${email}\nWhatsApp: ${whatsapp}\nMotivo: ${motivo}\nMensagem: ${mensagem}`
   };
@@ -34,10 +35,11 @@ app.post('/send-email', (req, res) => {
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error("Erro ao enviar email:", error);
-      return res.status(500).send(error.toString());
+      res.status(500).send(error.toString());
+    } else {
+      console.log("Email enviado com sucesso:", info.response);
+      res.status(200).send('Email enviado com sucesso: ' + info.response);
     }
-    console.log("Email enviado com sucesso:", info.response);
-    res.status(200).send('Email enviado com sucesso: ' + info.response);
   });
 });
 
