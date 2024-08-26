85bc41c83de525cfa96d30af12b3cb3c251536f4
thiago laredo <thiago@olatu.com.br>
2024-08-26 10:52:04 -0300

    tradução implementada e conteudo dinamico e submenu

--- src/js/modules/serviceLoader.js
+++ src/js/modules/serviceLoader.js
@@ -1,46 +1,50 @@
+
 export class ServiceLoader {
-    constructor(data) {
-        this.data = data;
-    }
+  constructor(data, languageSwitcher) {
+      this.data = data;
+      this.languageSwitcher = languageSwitcher; // Adiciona a instância de LanguageSwitcher
+  }
 
-    loadService(serviceId, lang = 'pt') {
-        if (!this.data || !this.data[lang] || !this.data[lang].servico) {
-            console.error('Dados de serviço não disponíveis:', this.data);
-            return;
-        }
+  loadService(serviceId, lang = 'pt') {
+      if (!this.data || !this.data[lang] || !this.data[lang].servico) {
+          console.error('Dados de serviço não disponíveis:', this.data);
+          return;
+      }
 
-        const serviceData = this.data[lang].servico[serviceId];
-        if (serviceData) {
-            this.displayService(serviceData, lang, serviceId);
-        } else {
-            console.error('Serviço não encontrado:', serviceId);
-        }
-    }
+      const serviceData = this.data[lang].servico[serviceId];
+      if (serviceData) {
+          this.displayService(serviceData, lang, serviceId);
+      } else {
+          console.error('Serviço não encontrado:', serviceId);
+      }
+  }
 
-    displayService(serviceData, lang, serviceId) {
-        const titleElement = document.getElementById('serviceTitle');
-        const descriptionElement = document.getElementById('serviceDescription');
-        const buttonElement = document.getElementById('serviceButton');
+  displayService(serviceData, lang, serviceId) {
+      const titleElement = document.getElementById('serviceTitle');
+      const descriptionElement = document.getElementById('serviceDescription');
+      const buttonElement = document.getElementById('serviceButton');
 
-        if (!serviceData) {
-            console.error('Dados de serviço não fornecidos');
-            return;
-        }
+      if (!serviceData) {
+          console.error('Dados de serviço não fornecidos');
+          return;
+      }
 
-        titleElement.textContent = serviceData.titulo;
-        descriptionElement.textContent = serviceData.descricao;
-        buttonElement.textContent = this.data[lang].servico.comum.servicoBotao;
+      // Atualizando o conteúdo diretamente
+      titleElement.textContent = serviceData.titulo;
+      descriptionElement.textContent = serviceData.descricao;
+      buttonElement.textContent = this.data[lang].servico.comum.servicoBotao;
 
-        if (serviceId) {
-            titleElement.setAttribute('data-translate', `servico.${serviceId}.titulo`);
-            descriptionElement.setAttribute('data-translate', `servico.${serviceId}.descricao`);
-            buttonElement.setAttribute('data-translate', 'servico.comum.servicoBotao');
-        } else {
-            console.error('Service ID is undefined or null');
-        }
+      if (serviceId) {
+          titleElement.setAttribute('data-translate', `servico.${serviceId}.titulo`);
+          descriptionElement.setAttribute('data-translate', `servico.${serviceId}.descricao`);
+          buttonElement.setAttribute('data-translate', 'servico.comum.servicoBotao');
+      } else {
+          console.error('Service ID is undefined or null');
+      }
 
-        if (typeof languageSwitcher !== 'undefined' && typeof languageSwitcher.applyTranslations === 'function') {
-            languageSwitcher.applyTranslations();
-        }
-    }
+      // Reaplica as traduções usando a instância de LanguageSwitcher
+      if (this.languageSwitcher && typeof this.languageSwitcher.applyTranslations === 'function') {
+          this.languageSwitcher.applyTranslations();
+      }
+  }
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
+
 import MenuMobile from './modules/menu-mobile.js';
 // import FormHandler from './modules/formHandler.js';
 import LanguageSwitcher from './modules/languageSwitcher.js';
-import translations from '../translations.json'
+import translations from '../translations.json';
 import { initAnimations } from './modules/animations.js';
 import { ServiceLoader } from './modules/serviceLoader.js';
-// import { adjustContainerWidth } from './modules/deviceStyleAdjuster.js';
-
+import { SubMenu } from './modules/subMenu.js';
 
 import "../css/global.css";
 import "../css/header.css";
@@ -23,39 +23,27 @@ import "../css/servicos.css";
 import "../css/rodape.css";
 import "../css/componentes.css";
 
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     console.log("DOM completamente carregado.");
-
-//     // Inicialização de componentes
-//     const menuMobile = new MenuMobile('[data-menu="logo"]', '[data-menu="button"]', '[data-menu="list"]', '[data-menu="contato-mobile"]', '[data-menu="linkedin"]');
-//     menuMobile.init();
-  
-//     // const formHandler = new FormHandler('contact-form');
-
-//     console.log("Iniciando animações GSAP...");
-  
-//     initAnimations();
-  
-//     console.log("Animações GSAP iniciadas.");
-  
-//     const languageSwitcher = new LanguageSwitcher(translations);  
-
-//     const serviceLoader = new ServiceLoader(translations);
-//     const serviceId = window.location.hash.substring(1);
-//     const currentLang = document.documentElement.lang || 'pt'; // Pega o idioma atual do documento
-
-//     if (serviceId) {
-//         serviceLoader.loadService(serviceId, currentLang);
-//     }
-   
-//   });
-  
-// Definição global de variáveis para garantir o acesso em funções de callback
-// Definição global para serviceId e currentLang
-// Definição global para serviceId e currentLang
 let serviceId = window.location.hash.substring(1);
 let currentLang = 'pt'; // Define um idioma padrão
+
+// Função para configurar os links dos serviços
+function setupServiceLinks(serviceLoader, lang) {
+    const serviceLinks = document.querySelectorAll('a[href*="servico.html"]');
+    serviceLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault(); // Impede o comportamento padrão do link
+            const serviceId = link.hash.substring(1); // Obtém o hash da URL (ID do serviço)
+
+            if (window.location.pathname.includes('servico.html')) {
+                // Se já estamos na página 'servico.html', apenas carrega o serviço dinamicamente
+                serviceLoader.loadService(serviceId, lang);
+            } else {
+                // Caso contrário, redireciona para 'servico.html' com o hash apropriado
+                window.location.href = `servico.html#${serviceId}`;
+            }
+        });
+    });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM completamente carregado.");
@@ -65,14 +53,20 @@ document.addEventListener('DOMContentLoaded', () => {
     menuMobile.init();
 
     initAnimations();
- // Definir currentLang corretamente aqui
- currentLang = 'pt'; // Força o idioma inicial como 'PT'
- const languageSwitcher = new LanguageSwitcher(translations, currentLang);
- const serviceLoader = new ServiceLoader(translations);
 
- if (serviceId) {
-     serviceLoader.loadService(serviceId, currentLang);
- } else {
-     console.error('Service ID is undefined or null on page load');
- }
+    const subMenu = new SubMenu('#menu');
+
+    // Definir currentLang corretamente aqui
+    currentLang = 'pt'; // Força o idioma inicial como 'PT'
+    const languageSwitcher = new LanguageSwitcher(translations, currentLang);
+    const serviceLoader = new ServiceLoader(translations, languageSwitcher);
+
+    if (serviceId) {
+        serviceLoader.loadService(serviceId, currentLang);
+    } else {
+        console.error('Service ID is undefined or null on page load');
+    }
+
+    // Chama a função para configurar os links de serviços
+    setupServiceLinks(serviceLoader, currentLang);
 });
