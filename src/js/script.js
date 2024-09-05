48f01507d83cce525bcdf46ea09c4ed1d9c08216
thiago laredo <thiago@olatu.com.br>
2024-09-05 14:41:23 -0300

    seo

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,96 @@
 
+// import MenuMobile from './modules/menu-mobile.js';
+// // import FormHandler from './modules/formHandler.js';
+// import LanguageSwitcher from './modules/languageSwitcher.js';
+// import translations from '../translations.json';
+// import { initPageLoadAnimations, initButtonAnimations, initScrollAnimations } from './modules/animations.js';
+// import { ServiceLoader } from './modules/serviceLoader.js';
+// import { SubMenu } from './modules/subMenu.js';
+
+// import "../css/global.css";
+// import "../css/header.css";
+// import "../css/introducao.css";
+// import "../css/introducao-small.css";
+// import "../css/text-typing.css";
+// import "../css/menu-mobile.css";
+// import "../css/cores.css";
+// import "../css/sobre.css";
+// import "../css/protecao.css";
+// import "../css/produtos.css";
+// import "../css/seguranca.css";
+// import "../css/fale-conosco.css";
+// import "../css/fique-alerta.css";
+// import "../css/servicos.css";
+// import "../css/servico.css";
+// import "../css/rodape.css";
+// import "../css/componentes.css";
+
+// let serviceId = window.location.hash.substring(1);
+// let currentLang = 'pt'; // Define um idioma padrão
+
+// // Função para configurar os links dos serviços
+// function setupServiceLinks(serviceLoader, lang) {
+//     const serviceLinks = document.querySelectorAll('a[href*="servico.html"]');
+//     serviceLinks.forEach(link => {
+//         link.addEventListener('click', (event) => {
+//             event.preventDefault(); // Impede o comportamento padrão do link
+//             const serviceId = link.hash.substring(1); // Obtém o hash da URL (ID do serviço)
+
+//             if (window.location.pathname.includes('servico.html')) {
+//                 // Se já estamos na página 'servico.html', apenas carrega o serviço dinamicamente
+//                 serviceLoader.loadService(serviceId, lang);
+//             } else {
+//                 // Caso contrário, redireciona para 'servico.html' com o hash apropriado
+//                 window.location.href = `servico.html#${serviceId}`;
+//             }
+//         });
+//     });
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log("DOM completamente carregado.");
+
+//   const menuMobile = new MenuMobile('[data-menu="logo"]', '[data-menu="button"]', '[data-menu="list"]', '[data-menu="contato-mobile"]', '[data-menu="linkedin"]');
+//   menuMobile.init();
+
+//   const subMenu = new SubMenu('#menu');
+
+//   // Definir currentLang corretamente aqui
+//   currentLang = 'pt'; // Força o idioma inicial como 'PT'
+//   const languageSwitcher = new LanguageSwitcher(translations, currentLang);
+//   languageSwitcher.applyTranslations();
+//   const serviceLoader = new ServiceLoader(translations, languageSwitcher);
+
+//   // Carrega o serviço baseado no hash atual na inicialização da página
+//   const serviceId = window.location.hash.substring(1); // Captura o ID do serviço do hash da URL
+//   if (serviceId) {
+//       serviceLoader.loadService(serviceId, currentLang);
+//   } else {
+//       console.error('Service ID is undefined or null on page load');
+//   }
+
+//   // Adiciona um listener para o evento de mudança de hash
+//   window.addEventListener('hashchange', () => {
+//       const newServiceId = window.location.hash.substring(1);
+//       if (newServiceId) {
+//           serviceLoader.loadService(newServiceId, currentLang);
+//       } else {
+//           console.error('Service ID is undefined or null on hash change');
+//       }
+//   });
+
+//   // Chama a função para configurar os links de serviços
+//   setupServiceLinks(serviceLoader, currentLang);
+// });
+
+// // Após o carregamento completo dos recursos, inicializa as animações e força a atualização do ScrollTrigger
+// window.addEventListener('load', () => {
+//     initPageLoadAnimations();
+//     initButtonAnimations();
+//     initScrollAnimations();
+// });
+
+
 import MenuMobile from './modules/menu-mobile.js';
 // import FormHandler from './modules/formHandler.js';
 import LanguageSwitcher from './modules/languageSwitcher.js';
@@ -7,6 +99,7 @@ import { initPageLoadAnimations, initButtonAnimations, initScrollAnimations } fr
 import { ServiceLoader } from './modules/serviceLoader.js';
 import { SubMenu } from './modules/subMenu.js';
 
+// Importação de CSS
 import "../css/global.css";
 import "../css/header.css";
 import "../css/introducao.css";
@@ -25,8 +118,8 @@ import "../css/servico.css";
 import "../css/rodape.css";
 import "../css/componentes.css";
 
-let serviceId = window.location.hash.substring(1);
-let currentLang = 'pt'; // Define um idioma padrão
+// Definição de variáveis globais
+let currentLang = 'pt'; // Define o idioma padrão
 
 // Função para configurar os links dos serviços
 function setupServiceLinks(serviceLoader, lang) {
@@ -47,35 +140,42 @@ function setupServiceLinks(serviceLoader, lang) {
     });
 }
 
+// Função principal de inicialização
 document.addEventListener('DOMContentLoaded', () => {
   console.log("DOM completamente carregado.");
 
+  // Inicializa o menu mobile
   const menuMobile = new MenuMobile('[data-menu="logo"]', '[data-menu="button"]', '[data-menu="list"]', '[data-menu="contato-mobile"]', '[data-menu="linkedin"]');
   menuMobile.init();
 
+  // Inicializa o submenu
   const subMenu = new SubMenu('#menu');
 
-  // Definir currentLang corretamente aqui
-  currentLang = 'pt'; // Força o idioma inicial como 'PT'
+  // Inicializa o trocador de idiomas e aplica as traduções
   const languageSwitcher = new LanguageSwitcher(translations, currentLang);
   languageSwitcher.applyTranslations();
+
+  // Inicializa o carregador de serviços
   const serviceLoader = new ServiceLoader(translations, languageSwitcher);
 
-  // Carrega o serviço baseado no hash atual na inicialização da página
+  // Carrega o serviço baseado no hash atual da URL na inicialização da página
   const serviceId = window.location.hash.substring(1); // Captura o ID do serviço do hash da URL
   if (serviceId) {
+      console.log('Carregando serviço com ID:', serviceId);
       serviceLoader.loadService(serviceId, currentLang);
   } else {
-      console.error('Service ID is undefined or null on page load');
+      console.info('Nenhum Service ID encontrado na URL. Carregando conteúdo padrão...');
+      // Opcional: Carregar um conteúdo padrão ou uma mensagem amigável para o usuário
   }
 
   // Adiciona um listener para o evento de mudança de hash
   window.addEventListener('hashchange', () => {
       const newServiceId = window.location.hash.substring(1);
       if (newServiceId) {
+          console.log('Hash changed, carregando novo serviço com ID:', newServiceId);
           serviceLoader.loadService(newServiceId, currentLang);
       } else {
-          console.error('Service ID is undefined or null on hash change');
+          console.warn('Service ID is undefined or null on hash change');
       }
   });
 
@@ -83,9 +183,10 @@ document.addEventListener('DOMContentLoaded', () => {
   setupServiceLinks(serviceLoader, currentLang);
 });
 
-// Após o carregamento completo dos recursos, inicializa as animações e força a atualização do ScrollTrigger
+// Inicializa as animações após o carregamento completo dos recursos
 window.addEventListener('load', () => {
     initPageLoadAnimations();
     initButtonAnimations();
     initScrollAnimations();
 });
+``
